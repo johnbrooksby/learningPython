a2b40bcb263debd267126a5976bcf18d5a40a83b
Anna Brooksby <johnhbrooksby@yahoo.com>
2025-01-26 22:48:09 -0700

    Continuing learing - Comparisons, Variable, Inputs

---
+++ math.py
@@ -0,0 +1,29 @@
+print (3 + 2)
+
+#F-strings
+print(f"3 + 2 = {3 + 2}")
+print(f"5 - 1 = {5 - 1}")
+print(f"3 * 4 = {3 * 4}")
+print(f"10 / 5 = {10 / 5}")
+print(f"35 % 3 = {35 % 3}")
+print(f"2 ** 4 = {2 ** 4}")
+
+#Comparison operators
+print(5 > 1)
+print(f"5 == 1: {5 == 1}")
+print(f"5 != 1: {5 != 1}")
+print(f"5 > 1: {5 > 1}")
+print(f"5 >= 1: {5 >= 1}")
+print(f"5 < 1: {5 < 1}")
+print(f"5 <= 1: {5 <= 1}")
+
+#Variable assignment
+my_name = "John Brooksby"
+print(my_name)
+
+#Input
+name = input("What's Your Name: ")
+print(f'Hello {name}')
+num1 = input("Enter A Number: ")
+num2 = input("Enter Another Number: ")
+print(f'{num1} + {num2} = {int(num1)+int(num2)}')
