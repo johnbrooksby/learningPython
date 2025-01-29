11d054adf2093660fcac1719454e6e73ff4d7e97
Anna Brooksby <johnhbrooksby@yahoo.com>
2025-01-28 21:58:43 -0700

    Daily lesson - Choose adventure

--- math.py
+++ math.py
@@ -1,25 +1,25 @@
-print (3 + 2)
+# print(3 + 2)
 
 #F-strings
-print(f"3 + 2 = {3 + 2}")
-print(f"5 - 1 = {5 - 1}")
-print(f"3 * 4 = {3 * 4}")
-print(f"10 / 5 = {10 / 5}")
-print(f"35 % 3 = {35 % 3}")
-print(f"2 ** 4 = {2 ** 4}")
+# print(f"3 + 2 = {3 + 2}")
+# print(f"5 - 1 = {5 - 1}")
+# print(f"3 * 4 = {3 * 4}")
+# print(f"10 / 5 = {10 / 5}")
+# print(f"35 % 3 = {35 % 3}")
+# print(f"2 ** 4 = {2 ** 4}")
 
 #Comparison operators
-print(5 > 1)
-print(f"5 == 1: {5 == 1}")
-print(f"5 != 1: {5 != 1}")
-print(f"5 > 1: {5 > 1}")
-print(f"5 >= 1: {5 >= 1}")
-print(f"5 < 1: {5 < 1}")
-print(f"5 <= 1: {5 <= 1}")
+# print(5 > 1)
+# print(f"5 == 1: {5 == 1}")
+# print(f"5 != 1: {5 != 1}")
+# print(f"5 > 1: {5 > 1}")
+# print(f"5 >= 1: {5 >= 1}")
+# print(f"5 < 1: {5 < 1}")
+# print(f"5 <= 1: {5 <= 1}")
 
 #Variable assignment
-my_name = "John Brooksby"
-print(my_name)
+# my_name = "John Brooksby"
+# print(my_name)
 
 #Input
 # name = input("What's Your Name: ")
@@ -30,11 +30,33 @@ print(my_name)
 # print(f'{num1} + {num2} = {int(num1)+int(num2)}')
 
 #IF / ELIF / ELSE
-x = input("Guess a number ")
-print (x)
-if int(x) == 41:
-    print("X Does in fact equal 41!")
-elif int(x) > 41:
-    print("Too high!")
+# x = input("Guess a number ")
+# print (x)
+# if int(x) == 41:
+#     print("X Does in fact equal 41!")
+# elif int(x) > 41:
+#     print("Too high!")
+# else:
+#     print("Too low")
+
+#Choose your own adventure sandbox
+from os import system
+system("clear")
+print("Welcome To Choose Your Own Adventure!")
+print("The goal is to find the Python Princess...")
+name = input("Enter Your Name: ")
+name = name.lower()
+system("clear")
+print("You're standing in front of two doors...")
+print("Do you want the door on the left or right?")
+question = input().lower()
+while(question != "left" and question != "right"):
+    print("Not a valid answer.  Try again: ")
+    question = input().lower()
+if question == "left":
+    system("clear")
+    print("You fell into a pit and died! GAME OVER")
 else:
-    print("Too low")
+    system("clear")
+    print(f"Congratulations {name.capitalize()} you found")
+    print("the Python Princess! YOU WIN!")
